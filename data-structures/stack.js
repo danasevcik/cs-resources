7ede037157213ac7f7c91606a56b7d7a4f5888ca
Dana Sevcik <danasevcik@hotmail.com>
2019-10-01 10:21:29 -0400

    define a stack

---
+++ data-structures/stack.js
@@ -0,0 +1,6 @@
+// A stack is an array or list structure with two principal operations:
+  // push
+    // add an element to the end of the stack
+  // pop
+    // remove an element from the end of the stack
+// LIFO is the guiding principle of a stack - last in, first out
