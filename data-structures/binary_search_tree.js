221a0b0f36ad52aa7b685276a8841a662375d824
Dana Sevcik <danasevcik@hotmail.com>
2019-10-08 13:53:51 -0400

    adds binary search tree description

---
+++ data-structures/binary_search_tree.js
@@ -0,0 +1,4 @@
+// A Binary tree is a type of tree in which each node has a left and right property
+// A Binary Search Tree is a type of binary tree
+// Nodes in a binary search tree have lesser values stored on the left
+// Nodes in a binary search tree have greater values stored on the right
