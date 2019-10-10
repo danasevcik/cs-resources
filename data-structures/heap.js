5612340f912b435872db11699f73c8760e656838
Dana Sevcik <danasevcik@hotmail.com>
2019-10-10 10:57:46 -0400

    heap definition

---
+++ data-structures/heap.js
@@ -0,0 +1,8 @@
+// A heap is a special tree data structure where the tree is a complete binary tree
+// There are two general types of heaps:
+  // Max-heap:
+    // The key present at the root node must be the greatest among all keys
+    // The same is true for all of the children nodes
+  // Min-heap:
+    // The key present at the root node must be the least among all keys
+    // The same is true for all of the children nodes
