587ed8f51186b1a77db318e80f026c42f2e3caad
Dana Sevcik <danasevcik@hotmail.com>
2019-10-10 11:15:12 -0400

    basic skeleton of minheap implementation

--- data-structures/heap.js
+++ data-structures/heap.js
@@ -6,3 +6,21 @@
   // Min-heap:
     // The key present at the root node must be the least among all keys
     // The same is true for all of the children nodes
+
+class MinHeap {
+  constructor(root=null) {
+    this.root = root;
+  }
+
+  insert(node, key) {
+    // insert key given a node
+  }
+}
+
+class Node {
+  constructor(left=null, right=null, key) {
+    this.left = left;
+    this.right = right;
+    this.key = key;
+  }
+}
