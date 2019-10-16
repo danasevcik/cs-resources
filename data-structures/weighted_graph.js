b46ae84b9e4fb60d04a95d96dc19718551b61c88
Dana Sevcik <danasevcik@hotmail.com>
2019-10-16 10:48:17 -0400

    comments on weighted graphs

---
+++ data-structures/weighted_graph.js
@@ -0,0 +1,2 @@
+// A weighted graph is a type of graph (can be directed or undirected) in which each branch is given a numerical weight.
+// A weighted graph is a special type of labeled graph in which the labels are usually positive numbers
