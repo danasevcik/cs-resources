fb53cbeb425145ed8b4df3b24caa470f8e356541
Dana Sevcik <danasevcik@hotmail.com>
2019-10-16 11:21:21 -0400

    basic weighted graph and node class

--- data-structures/weighted_graph.js
+++ data-structures/weighted_graph.js
@@ -1,2 +1,17 @@
 // A weighted graph is a type of graph (can be directed or undirected) in which each branch is given a numerical weight.
 // A weighted graph is a special type of labeled graph in which the labels are usually positive numbers
+
+class WeightedGraph {
+  constructor(undirected = true) {
+    this.adjList = new Map();
+    this.undirected = undirected;
+  }
+
+  
+}
+
+class Node {
+  constructor(data) {
+    this.data = data;
+  }
+}
