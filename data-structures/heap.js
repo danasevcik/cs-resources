d85bb3565140e17e8aedc555403b67ac9069b6d4
Dana Sevcik <danasevcik@hotmail.com>
2019-10-25 08:53:13 -0400

    starts remove head function

--- data-structures/heap.js
+++ data-structures/heap.js
@@ -12,4 +12,17 @@ class Heap {
     // bubble up based on index and value that was just added
     this.bubbleUp(this.data.length - 1, value);
   }
+
+  removeHead() {
+    // access first and last elements of the heap
+    let head = this.data[0];
+    let tail = this.data.pop();
+
+    if (this.data[0]) {
+      this.data[0] = tail;
+
+      this.bubbleDown(0, tail);
+    }
+    return head;
+  }
 }
