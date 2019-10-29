1604850b15a9a4641f675cf91c4b01443252656c
Dana Sevcik <danasevcik@hotmail.com>
2019-10-29 14:46:16 -0400

    selection sort

--- data-structures/sorting_alogrithms.js
+++ data-structures/sorting_alogrithms.js
@@ -3,9 +3,9 @@
 
 function bubbleSort(arr) {
   // get the length of the arr and iterate twice through the arr
-  let currentLength = arr.length;
-  for (let i = 0; i < currentLength; i++) {
-    for (let j = 0; j < currentLength; j++) {
+  let length = arr.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
       // if the second is greater, move it to the right
       if (arr[j] > arr[i]) {
         let currentEle = arr[j];
@@ -20,6 +20,31 @@ function bubbleSort(arr) {
 
 // selection sort
 // runs O(n)
+
+function selectionSort(arr) {
+  // get the length of the arr and iterate twice through the arr
+  let length = arr.length;
+  for (let i = 0; i < length; i++) {
+    // assume the min is 0
+    let min = i;
+    for (let j = i + 1; j < length; j++) {
+      // if the arr at index 0 is greater than arr[j]
+      // reset min
+      if (arr[min] > arr[j]) {
+        min = j;
+      }
+      // if the first ele is not the min
+      // move current ele to the left
+      if (!min == i) {
+        let currentEle = arr[i];
+        arr[i] = arr[min];
+        arr[min] = currentEle;
+      }
+    }
+  }
+  // return sorted array
+  return arr;
+}
 
 // merge sort
 // runs O(n*log(n))
