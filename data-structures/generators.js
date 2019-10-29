de22324a11ae92b354ce77c3099e7b0a5c6a8cdc
Dana Sevcik <danasevcik@hotmail.com>
2019-10-29 14:02:46 -0400

    general generator desc

---
+++ data-structures/generators.js
@@ -0,0 +1,5 @@
+// generators simplify iterator-authoring using function* and yield.
+// A function declared as function* returns a Generator instance.
+// Generators are subtypes of iterators which include additional next and throw.
+// These enable values to flow back into the generator, so yield is an expression form which returns a value (or throws)
+// Can also be used to enable 'await'-like async programming.
