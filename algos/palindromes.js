ab1442287fe3abf49b2379bff5a21eef7cfb2f8a
Dana Sevcik <danasevcik@hotmail.com>
2019-11-13 15:51:27 -0500

    palindrome solution

--- algos/palindromes.js
+++ algos/palindromes.js
@@ -9,12 +9,41 @@
 
 // check if strings are an even length
 // if they are, split in half
+// iterate over one half
 // access first and last element of halves
 // if they dont match, false
 // if they all match, return true
 
 function palindrome(str) {
-  if (str1.length % 2 === 0) {
-    
+  if (str.length % 2 !== 0) {
+    let midpoint = Math.floor(str.length / 2);
+    let firstHalf = str.slice(0, midpoint);
+    let secondHalf = str.slice(midpoint + 1);
+    let firstLength = firstHalf.length;
+
+    for(let i = 0; i < firstLength; i++) {
+      if (firstHalf[i] !== secondHalf[secondHalf.length - (i + 1)]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    let midpoint = str.length / 2;
+    let firstHalf = str.slice(0, midpoint);
+    let secondHalf = str.slice(midpoint);
+    let firstLength = firstHalf.length;
+
+    for(let i = 0; i < firstLength; i++) {
+      if (firstHalf[i] !== secondHalf[secondHalf.length - (i + 1)]) {
+        return false;
+      }
+    }
+    return true;
   }
 }
+
+console.log(palindrome('abcba')); //true
+console.log(palindrome('abcdba')); // false
+console.log(palindrome('ab')); // false
+console.log(palindrome('abba')); // true
+console.log(palindrome('a')); // true
