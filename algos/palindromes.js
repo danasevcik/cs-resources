83b0c8ba922cd660e14d0b14c909b1541b643cb0
Dana Sevcik <danasevcik@hotmail.com>
2019-11-13 15:34:57 -0500

    comments for palindrome

---
+++ algos/palindromes.js
@@ -0,0 +1,20 @@
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+// check if strings are an even length
+// if they are, split in half
+// access first and last element of halves
+// if they dont match, false
+// if they all match, return true
+
+function palindrome(str) {
+  if (str1.length % 2 === 0) {
+    
+  }
+}
