9d35d488d4c5b9a4d870bf43d4283d89a5deef97
Dana Sevcik <danasevcik@hotmail.com>
2019-11-14 10:40:28 -0500

    reverse int function and test cases

--- algos/integer_reversal.js
+++ algos/integer_reversal.js
@@ -12,3 +12,20 @@
 // or
 // check if first ele is a '-' sign
 // loop backwards and add int to new variable
+
+function reverseInt(num) {
+  let numStr = num.toString()
+  if (numStr[0] === '-') {
+    let reverseNumStr = numStr.slice(1).split('').reverse().join('');
+    reverseNumStr = '-' + reverseNumStr;
+    return parseInt(reverseNumStr)
+  } else {
+    let reverseNumStr = numStr.split('').reverse().join('');
+    return parseInt(reverseNumStr)
+  }
+}
+
+console.log(reverseInt(-12)); // -21
+console.log(reverseInt(12)); // 21
+console.log(reverseInt(100)); // 1
+console.log(reverseInt(1289)); // 9821
