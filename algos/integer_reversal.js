0cb9cd38dd6e97672d3e9f97c86a8bba50db9ac6
Dana Sevcik <danasevcik@hotmail.com>
2019-11-14 10:00:25 -0500

    comments for integer reversal

---
+++ algos/integer_reversal.js
@@ -0,0 +1,14 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+// can either use .reverse on a split array
+// or
+// check if first ele is a '-' sign
+// loop backwards and add int to new variable
