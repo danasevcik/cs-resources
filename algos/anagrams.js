24041a1bbfde4d00dccc616418ffc78d2d8e9c60
Dana Sevcik <danasevcik@hotmail.com>
2019-11-20 15:33:41 -0500

    anagram commented solution

---
+++ algos/anagrams.js
@@ -0,0 +1,15 @@
+// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+
+// check that strings are same length
+// create char map of one string
+// iterate through other string and decrement value at given key
+// if all values are 0, return true
+// otherwise return false
