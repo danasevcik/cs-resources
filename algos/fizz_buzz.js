ac042d9fccc91b7396b7b968ab579455ad06c954
Dana Sevcik <danasevcik@hotmail.com>
2019-11-20 14:26:12 -0500

    fizzbuzz

---
+++ algos/fizz_buzz.js
@@ -0,0 +1,34 @@
+// --- Directions
+// Write a program that console logs the numbers
+// from 1 to n. But for multiples of three print
+// “fizz” instead of the number and for the multiples
+// of five print “buzz”. For numbers which are multiples
+// of both three and five print “fizzbuzz”.
+// --- Example
+//   fizzBuzz(5);
+//   1
+//   2
+//   fizz
+//   4
+//   buzz
+
+// iterate from 1 to number
+// check if num is divisible by 3 and 5
+// then 3 then 5
+// print result
+
+function fizzBuzz(num) {
+  for (let i = 1; i <= num; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else {
+      console.log(i)
+    }
+  }
+}
+
+console.log(fizzBuzz(5))
--- algos/max_chars.js
+++ algos/max_chars.js
@@ -33,5 +33,4 @@ function maxChars(str) {
   }
 
   return result;
-
 }
