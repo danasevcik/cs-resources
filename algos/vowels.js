a564c1dc07015a22d8c414acd35975e595886682
Dana Sevcik <danasevcik@hotmail.com>
2019-11-22 11:56:04 -0500

    vowels commented solution

---
+++ algos/vowels.js
@@ -0,0 +1,12 @@
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+// create a hash of vowels
+// iterate through string
+// if the char is a vowel, incrememnt counter 
