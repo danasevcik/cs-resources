7bba4d4b2986362404bb6ab20efe7da22e71c8f5
Dana Sevcik <danasevcik@hotmail.com>
2019-11-22 12:00:02 -0500

    vowel count solution

--- algos/vowels.js
+++ algos/vowels.js
@@ -9,4 +9,29 @@
 
 // create a hash of vowels
 // iterate through string
-// if the char is a vowel, incrememnt counter 
+// if the char is a vowel, incrememnt counter
+
+function vowelCount(str) {
+  let vowels = {
+    a: true,
+    e: true,
+    i: true,
+    o: true,
+    u: true
+  };
+
+  let numOfVowels = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i];
+
+    if (vowels[char]) {
+      numOfVowels++;
+    }
+  }
+
+  return numOfVowels;
+}
+
+console.log(vowelCount('hiii')) // 3
+console.log(vowelCount('hey how are you')) // 6
