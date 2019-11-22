cc3941ea496988f60d895e3580e93e680de4c31f
Dana Sevcik <danasevcik@hotmail.com>
2019-11-22 09:37:50 -0500

    comment solution for printing steps

---
+++ algos/printing_steps.js
@@ -0,0 +1,21 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+// loop twice - first for the row, and again for the characters in that row
+// on the second iteration, check if j is less than or equal to n
