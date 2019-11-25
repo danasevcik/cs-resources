be70f19ae99bdcc9eb220c8cbe20bd47eb00e404
Dana Sevcik <danasevcik@hotmail.com>
2019-11-25 11:23:10 -0500

    fibonacci recursive solution

--- algos/fibonacci.js
+++ algos/fibonacci.js
@@ -14,22 +14,40 @@
 // otherwise, add n-1 and n-2 together
 // print out the n number of fib
 
-function fibonacci(n) {
-  let fibArr = [];
+// function fibonacci(n) {
+//   let fibArr = [];
+//
+//   for (let i = 0; i <= n; i++) {
+//     if (i === 0  || i === 1) {
+//       fibArr.push(i);
+//     } else {
+//       fibArr.push(fibArr[i - 2] + fibArr[i - 1]);
+//     }
+//   }
+//
+//   return fibArr[n];
+// }
+//
+// console.log(fibonacci(4)) // 3
+// console.log(fibonacci(5)) // 5
+// console.log(fibonacci(6)) // 8
+// console.log(fibonacci(7)) // 13
+// console.log(fibonacci(8)) // 21
 
-  for (let i = 0; i <= n; i++) {
-    if (i === 0  || i === 1) {
-      fibArr.push(i);
-    } else {
-      fibArr.push(fibArr[i - 2] + fibArr[i - 1]);
-    }
+// recursive solution
+// start with array and have base case be 0 or 1
+// otherwise, call fib with n-1 and n-2
+
+function fibRecursive(n) {
+  if (n === 0 || n === 1) {
+    return n;
+  } else {
+    return fibRecursive(n - 1) + fibRecursive(n - 2);
   }
-
-  return fibArr[n];
 }
 
-console.log(fibonacci(4)) // 3
-console.log(fibonacci(5)) // 5
-console.log(fibonacci(6)) // 8
-console.log(fibonacci(7)) // 13
-console.log(fibonacci(8)) // 21
+console.log(fibRecursive(4)) // 3
+console.log(fibRecursive(5)) // 5
+console.log(fibRecursive(6)) // 8
+console.log(fibRecursive(7)) // 13
+console.log(fibRecursive(8)) // 21
