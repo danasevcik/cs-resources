a2b79176d26fb8392b9a279494328d1b52398ad4
Dana Sevcik <danasevcik@hotmail.com>
2019-11-25 16:26:43 -0500

    stack solution

--- algos/stack.js
+++ algos/stack.js
@@ -15,3 +15,34 @@
 // push - use .push
 // pop - use .pop
 // peek - look at first index
+
+class Stack {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(element) {
+    return this.stack.push(element);
+  }
+
+  pop() {
+    return this.stack.pop();
+  }
+
+  peek() {
+    if (this.stack.length) {
+      return this.stack[0]
+    }
+  }
+}
+
+let stack = new Stack;
+stack.push(1);
+console.log(stack.stack);
+stack.pop();
+console.log(stack.stack);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+console.log(stack.stack);
+console.log(stack.peek());
