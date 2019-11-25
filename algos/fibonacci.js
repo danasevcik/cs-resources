e453e66b480b87eafdf41690886b344e385ea538
Dana Sevcik <danasevcik@hotmail.com>
2019-11-25 10:51:12 -0500

    fib solution commented

---
+++ algos/fibonacci.js
@@ -0,0 +1,19 @@
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+// create fib array
+// loop from 0 to num
+// if 0 or 1, add n to the arr
+// otherwise, add n-1 and n-2 together
+// print out the n number of fib
+
+function fibonacci(n) {
+  
+}
