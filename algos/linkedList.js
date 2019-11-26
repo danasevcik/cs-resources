4c30ebeb93a4ca23eadfbe8d38c8f36c0c644ad6
Dana Sevcik <danasevcik@hotmail.com>
2019-11-26 10:39:55 -0500

    simple linked list implementation

---
+++ algos/linkedList.js
@@ -0,0 +1,16 @@
+// --- Directions
+// Implement classes Node and Linked Lists
+// See 'directions' document
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(head) {
+    this.head = head;
+  }
+}
