bd1128c2a58a98cf76ab063b0f4a9d3905a566b9
Dana Sevcik <danasevcik@hotmail.com>
2019-11-29 11:16:12 -0500

    change node constructor to accept next property

--- algos/insertFirst.js
+++ algos/insertFirst.js
@@ -14,18 +14,30 @@
 // otherwise point the next property to the current first and change head
 
 class Node {
-  constructor(data) {
+  constructor(data, next=null) {
     this.data = data;
-    this.next = null;
+    this.next = next;
   }
 }
 
 class LinkedList {
-  constructor(head) {
+  constructor(head=null) {
     this.head = head;
   }
 
   insertFirst(data) {
-
+    let node = new Node(data)
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      let curr = this.head;
+      this.head = node;
+      this.head.next = curr;
+    }
   }
+
 }
+let nodeTwo = new Node(2);
+let nodeOne = new Node(1, nodeTwo);
+let list = new LinkedList(nodeOne);
+console.log(list);
