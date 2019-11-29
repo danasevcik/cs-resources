3312395917caa0a7d97a3fb434463727e016b158
Dana Sevcik <danasevcik@hotmail.com>
2019-11-29 12:13:47 -0500

    get first linked list commented solution

---
+++ algos/linkedListGetFirst.js
@@ -0,0 +1,25 @@
+// describe.skip('GetFirst', () => {
+//   test('returns the first element', () => {
+//     const l = new List();
+//     l.insertFirst(1);
+//     expect(l.getFirst().data).toEqual(1);
+//     l.insertFirst(2);
+//     expect(l.getFirst().data).toEqual(2);
+//   });
+// });
+
+// if there is no head, return null
+// otherwise, return head
+
+class Node {
+  constructor(data, next=null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor(head) {
+    this.head = head;
+  }
+}
