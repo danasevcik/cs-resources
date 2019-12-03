491e8218485ad71921e97623c827a2449c9668a0
Dana Sevcik <danasevcik@hotmail.com>
2019-12-03 18:01:07 -0500

    linked list get at commented solution

---
+++ algos/linkedListGetAt.js
@@ -0,0 +1,20 @@
+// describe.skip('GetAt', () => {
+//   test('returns the node at given index', () => {
+//     const l = new List();
+//     expect(l.getAt(10)).toEqual(null);
+//
+//     l.insertLast(1);
+//     l.insertLast(2);
+//     l.insertLast(3);
+//     l.insertLast(4);
+//
+//     expect(l.getAt(0).data).toEqual(1);
+//     expect(l.getAt(1).data).toEqual(2);
+//     expect(l.getAt(2).data).toEqual(3);
+//     expect(l.getAt(3).data).toEqual(4);
+//   });
+// });
+
+// iterate through the list and add to index counter
+// when arrived at num, return node's data
+// if nothing, return null
