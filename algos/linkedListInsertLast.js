a6abc2d090527cec5923a3cbce565dad62f15c1d
Dana Sevcik <danasevcik@hotmail.com>
2019-12-03 17:51:26 -0500

    commented solution for insert last

---
+++ algos/linkedListInsertLast.js
@@ -0,0 +1,15 @@
+// describe.skip('InsertLast', () => {
+//   test('adds to the end of the list', () => {
+//     const l = new List();
+//     l.insertFirst('a');
+//
+//     l.insertLast('b');
+//
+//     expect(l.size()).toEqual(2);
+//     expect(l.getLast().data).toEqual('b');
+//   });
+// });
+
+// iterate until the end of the list
+// point the last node to the newly created node
+// return that new node
