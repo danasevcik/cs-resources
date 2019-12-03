b34fa20a997e7ff43a7425a01fc94f93427cd806
Dana Sevcik <danasevcik@hotmail.com>
2019-12-03 17:54:46 -0500

    insert last solution

--- algos/linkedListInsertLast.js
+++ algos/linkedListInsertLast.js
@@ -13,3 +13,41 @@
 // iterate until the end of the list
 // point the last node to the newly created node
 // return that new node
+
+class Node {
+  constructor(data, next=null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  constructor(head) {
+    this.head = head;
+  }
+
+  insertLast(data) {
+    let newNode = new Node(data);
+
+    if (this.head) {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+      return newNode;
+    } else {
+      this.head = newNode;
+      return newNode;
+    }
+  }
+}
+
+let nodeOne = new Node(1);
+let nodeTwo = new Node(2, nodeOne);
+let nodeThree = new Node(3, nodeTwo);
+let nodeFour = new Node(4, nodeThree);
+let list = new LinkedList(nodeFour);
+console.log(list);
+console.log(list.insertLast(7));
+console.log(list);
