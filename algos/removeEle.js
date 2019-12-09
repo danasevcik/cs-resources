3c43f8f8a3b56ac529c64fa91e7d3e506eab81ee
Dana Sevcik <danasevcik@hotmail.com>
2019-12-09 10:47:51 -0500

    commented solution for remove ele

---
+++ algos/removeEle.js
@@ -0,0 +1,7 @@
+// given array and num
+// remove all elements that match num
+// return length of updated array
+
+// iterate over arr
+// if the ele matches num, splice
+// at the end return arr.length
