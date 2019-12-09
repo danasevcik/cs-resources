45854c97dcc86d892c29b279d479b713de8b662a
Dana Sevcik <danasevcik@hotmail.com>
2019-12-09 09:40:09 -0500

    num palindrome commented solution

---
+++ algos/numPalindrome.js
@@ -0,0 +1,8 @@
+// return true or false if a number is a palindrome
+// 121 => true
+// -505 => false
+
+// iterate over number from 0 to mid
+// if first char is '-', return false
+// if the num does not match the num at other end, return false
+// otherwise return true
