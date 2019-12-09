69b38e5fc1090427ebbc26ceef91af29c07ea6b8
Dana Sevcik <danasevcik@hotmail.com>
2019-12-09 10:13:55 -0500

    roman to int commented solution

---
+++ algos/romanToInt.js
@@ -0,0 +1,10 @@
+// convert roman numeral to int
+// 'III' => 3
+// 'IV' => 4
+// "IX" => 9
+// "LVIII" => 58
+
+// create obj for roman numerals and corresponding numbers
+// check if the current char and the next are a key in obj
+// if not, add num for current char
+// return number
