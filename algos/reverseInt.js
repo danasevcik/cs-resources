59de40be9365888481cf374390f1c29518bdb1f2
Dana Sevcik <danasevcik@hotmail.com>
2019-12-09 10:08:53 -0500

    reverse int solution

--- algos/reverseInt.js
+++ algos/reverseInt.js
@@ -9,3 +9,31 @@
 // if str is empty or only has '-', dont add 0
 // otherwise add num
 // return num
+
+function reverseInt(num) {
+  let numStr = num.toString();
+  let result = '';
+
+  if (numStr[0] === '-') {
+    result += numStr[0];
+  }
+
+  for (let i = numStr.length - 1; i >= 0; i--) {
+    if (numStr[i] !== '-') {
+      if (result[0] !== '-' || result[1]) {
+        result += numStr[i];
+      } else if (result[0] === '-' && !result[1]) {
+        if (numStr[i] !== '0') {
+          result += numStr[i];
+        }
+      }
+    }
+  }
+
+  return parseInt(result);
+
+}
+
+console.log(reverseInt(123)); // 321
+console.log(reverseInt(-500)); // -5
+console.log(reverseInt(1534236469)); // 9646324351
