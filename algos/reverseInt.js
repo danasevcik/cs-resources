8d5526344466662bc594bff596f1e6e5a86b8ad2
Dana Sevcik <danasevcik@hotmail.com>
2019-12-09 09:54:48 -0500

    reverse int commented solution

---
+++ algos/reverseInt.js
@@ -0,0 +1,11 @@
+// reverse the int and return that num
+// 123 => 321
+// -500 => -5
+// 820 => 28
+
+// change num to string
+// if first char is '-', keep first
+// iterate from end of num
+// if str is empty or only has '-', dont add 0
+// otherwise add num
+// return num
