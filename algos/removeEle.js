57829dc73d8e56cc266bc0b43f2f2d25c76dd1ed
Dana Sevcik <danasevcik@hotmail.com>
2019-12-10 09:15:41 -0500

    remove ele from array solution

--- algos/removeEle.js
+++ algos/removeEle.js
@@ -5,3 +5,18 @@
 // iterate over arr
 // if the ele matches num, splice
 // at the end return arr.length
+
+function removeEle(arr, ele) {
+  for (let i = 0; i < arr.length; i++) {
+    let currChar = arr[i];
+    if (currChar === ele) {
+      arr.splice(i, 1);
+    }
+  }
+
+  return arr.length;
+}
+
+console.log(removeEle([1,2,3,4,2,3,4], 2)); // 5
+console.log(removeEle([2], 2)); // 0
+console.log(removeEle([2,3,2], 2)); // 1
