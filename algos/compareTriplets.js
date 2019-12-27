ec00a25430908b310985f1f4d3c1423e23bf5f13
Dana Sevcik <danasevcik@hotmail.com>
2019-12-27 16:05:21 -0500

    arguably faster solution, less readable for triplets

--- algos/compareTriplets.js
+++ algos/compareTriplets.js
@@ -22,7 +22,38 @@ function compareTriplets(arrA, arrB) {
   return result;
 }
 
+// 
+
+function compareTripletsFaster(arrA, arrB) {
+  let result = [0,0];
+
+  if (arrA[0] > arrB[0]) {
+    result[0] += 1
+  } else if (arrB[0] > arrA[0]) {
+    result[1] += 1
+  }
+
+  if (arrA[1] > arrB[1]) {
+    result[0] += 1
+  } else if (arrB[1] > arrA[1]) {
+    result[1] += 1
+  }
+
+  if (arrA[2] > arrB[2]) {
+    result[0] += 1
+  } else if (arrB[2] > arrA[2]) {
+    result[1] += 1
+  }
+
+  return result;
+}
+
 console.log(compareTriplets([1,2,3], [3,2,1]));
 console.log(compareTriplets([2,2,2], [1,1,1]));
 console.log(compareTriplets([1,1,1], [2,2,2]));
 console.log(compareTriplets([2,2,2], [2,2,2]));
+
+console.log(compareTripletsFaster([1,2,3], [3,2,1]));
+console.log(compareTripletsFaster([2,2,2], [1,1,1]));
+console.log(compareTripletsFaster([1,1,1], [2,2,2]));
+console.log(compareTripletsFaster([2,2,2], [2,2,2]));
