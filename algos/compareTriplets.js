8c031c5ecc78287668902f3bfbb534c3e8b6172b
Dana Sevcik <danasevcik@hotmail.com>
2019-12-27 15:47:06 -0500

    additional tests for compare triplets

--- algos/compareTriplets.js
+++ algos/compareTriplets.js
@@ -7,3 +7,22 @@
 // iterate through one of the arrays
 // compare two ele and add point accordingly
 // return arr
+
+function compareTriplets(arrA, arrB) {
+  let result = [0,0];
+
+  for (let i = 0; i < arrA.length; i++) {
+    if (arrA[i] > arrB[i]) {
+      result[0] += 1;
+    } else if (arrA[i] < arrB[i]) {
+      result[1] += 1;
+    }
+  }
+
+  return result;
+}
+
+console.log(compareTriplets([1,2,3], [3,2,1]));
+console.log(compareTriplets([2,2,2], [1,1,1]));
+console.log(compareTriplets([1,1,1], [2,2,2]));
+console.log(compareTriplets([2,2,2], [2,2,2]));
