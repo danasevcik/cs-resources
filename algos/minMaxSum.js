034fddeb5460c8b6933c135c920c9dc4a0c9243a
Dana Sevcik <danasevcik@hotmail.com>
2019-12-31 14:37:51 -0500

    min max sum problem commented solution

---
+++ algos/minMaxSum.js
@@ -0,0 +1,7 @@
+// given arr of 5 int
+// find the max sum from exactly 4 of the 5 int
+// find the min sum from exactly 4 of the 5 int
+
+// sort the arr
+// for min, sum together first 4 elements
+// for max, sum last 4 elements (slice from 1?)
