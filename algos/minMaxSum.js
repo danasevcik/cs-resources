768c6c8411dff85ac44db45c9cbcfd6e80b96087
Dana Sevcik <danasevcik@hotmail.com>
2020-01-02 11:52:39 -0500

    min max sum solution

--- algos/minMaxSum.js
+++ algos/minMaxSum.js
@@ -10,7 +10,18 @@ function minMax(arr) {
   let sortedArr = arr.sort((a,b) => {
     return a - b;
   })
-  console.log(sortedArr);
+
+  let min = sortedArr.slice(0,4);
+  let max = sortedArr.slice(1);
+
+  function sum(total, num) {
+    return total += num
+  }
+
+  console.log(min.reduce(sum));
+  console.log(max.reduce(sum));
 }
 
-console.log(minMax([2,5,1,3,4]));
+console.log(minMax([2, 5, 1, 3, 4]));
+console.log(minMax([10, 10, 20, 20, 30]));
+console.log(minMax([5, 6, 7, 10, 9]));
