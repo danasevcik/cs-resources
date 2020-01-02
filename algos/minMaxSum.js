d17f19907420ac4e417491c75d458b15712bdf3e
Dana Sevcik <danasevcik@hotmail.com>
2020-01-02 10:00:58 -0500

    sorted arr for min max sum

--- algos/minMaxSum.js
+++ algos/minMaxSum.js
@@ -5,3 +5,12 @@
 // sort the arr
 // for min, sum together first 4 elements
 // for max, sum last 4 elements (slice from 1?)
+
+function minMax(arr) {
+  let sortedArr = arr.sort((a,b) => {
+    return a - b;
+  })
+  console.log(sortedArr);
+}
+
+console.log(minMax([2,5,1,3,4]));
