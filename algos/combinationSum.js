a2295196ad01a298e1c337560a9a58e4077d51c3
Dana Sevcik <danasevcik@hotmail.com>
2020-01-03 08:53:48 -0500

    commented solution for combination sum

---
+++ algos/combinationSum.js
@@ -0,0 +1,9 @@
+// given unique arr of numbers and a target
+// find all unique combinations
+
+// create solution set arr
+// create temp arr
+// iterate twice and check always if the sum has been reached
+// if not, keep iterating
+// if yes, push into solution arr
+// return solution arr
