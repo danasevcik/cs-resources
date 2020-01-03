f510f6e72382fee8c756ee578b47a5abe79161c4
Dana Sevcik <danasevcik@hotmail.com>
2020-01-03 10:20:47 -0500

    initial solution with repeating values

--- algos/combinationSum.js
+++ algos/combinationSum.js
@@ -7,3 +7,41 @@
 // if not, keep iterating
 // if yes, push into solution arr
 // return solution arr
+
+function combinationSum(arr, target) {
+  let solution = [];
+  let currentCombo = [];
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    currentCombo.push(arr[i]);
+    sum += arr[i];
+    console.log('sum', sum);
+    console.log('currentCombo', currentCombo);
+    if (checkSum(sum, currentCombo, solution, target)) {
+      solution.push(currentCombo)
+      currentCombo = [];
+      sum = 0;
+    }
+    for (let j = 1; j < arr.length; j++) {
+      currentCombo.push(arr[j]);
+      sum += arr[j];
+      if (checkSum(sum, currentCombo, solution, target)) {
+        solution.push(currentCombo)
+        currentCombo = [];
+        sum = 0;
+      }
+    }
+  }
+
+  return solution;
+}
+
+function checkSum(sum, currentCombo, solution, target) {
+  if (sum === target) {
+    solution.push(currentCombo);
+    return true;
+  }
+}
+
+console.log(combinationSum([1,2,3], 3)); // should return [[1,2], [3]]
