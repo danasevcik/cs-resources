bd12eee7319b860d55308d5a03d8e8fa51f7c9be
Dana Sevcik <danasevcik@hotmail.com>
2020-01-06 09:46:17 -0500

    circular list solution

--- algos/circularList.js
+++ algos/circularList.js
@@ -4,3 +4,23 @@
 // create a map where the the node points to true
 // if the next property is null, return false
 // otherwise check if the node has already been visited
+
+
+function circularList(head) {
+  let seen = new Map();
+  console.log(seen);
+
+  while (head.next) {
+    let current = head.next;
+    if (seen[current]) {
+      return true;
+    } else if (!seen[current]) {
+      seen[current] = true;
+    } else if (current === null) {
+      return false;
+    }
+  }
+  return false;
+}
+
+console.log(circularList('x'));
