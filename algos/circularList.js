2c9136b5531c362fb3016499ef3dc1b096a2f108
Dana Sevcik <danasevcik@hotmail.com>
2020-01-06 08:46:32 -0500

    commented solution for circular list

---
+++ algos/circularList.js
@@ -0,0 +1,6 @@
+// given a linked list head, determine if the list is circular
+
+// start iterating from the given head
+// create a map where the the node points to true
+// if the next property is null, return false
+// otherwise check if the node has already been visited
