f24a60c76ebe89635ce5f0827f8c218c7fe345db
Dana Sevcik <danasevcik@hotmail.com>
2020-01-07 10:39:37 -0500

    solution for most common with sorted arr

--- algos/mostCommon.js
+++ algos/mostCommon.js
@@ -6,3 +6,38 @@
 // create a count obj
 // also have a highest num and count key which keeps track of the highest
 // return the highest num
+
+function mostCommon(arr) {
+  let count = {
+    highestCount: 0,
+    highestNum: null
+  };
+
+  let sortedArr = arr.sort()
+
+  for (let i = 0; i < sortedArr.length; i++) {
+    let num = sortedArr[i];
+
+    if (count[num]) {
+      count[num]++
+      if (count[num] > count.highestCount) {
+        count.highestCount = count[num];
+        count.highestNum = num;
+      }
+    } else if (!count[num]) {
+      count[num] = 1;
+      if (count[num] > count.highestCount) {
+        count.highestCount = count[num];
+        count.highestNum = num;
+      }
+    }
+  }
+
+  return count.highestNum;
+}
+
+console.log(mostCommon([1,1,1,2])); // 1
+console.log(mostCommon([1,1,1,2,2,2,2,2,2])); // 2
+console.log(mostCommon([3,4])); // 3
+console.log(mostCommon([4,3])); // 3
+console.log(mostCommon([3,1,1,1])); // 1
