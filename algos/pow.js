9db511b5d6ad0496ab3d750221514e3bf3330424
Dana Sevcik <danasevcik@hotmail.com>
2020-01-08 10:26:17 -0500

    pow solution

--- algos/pow.js
+++ algos/pow.js
@@ -4,3 +4,20 @@
 // iterate n number of times
 // multiply x by itself that many times
 // return the number
+
+function pow(x, n) {
+  let i = 0;
+  let solution = 1;
+
+  while (i < n) {
+    solution = solution * x;
+
+    i++;
+  }
+
+  return solution;
+}
+
+console.log(pow(2, 2)); // 4
+console.log(pow(3, 2)); // 9
+console.log(pow(4, 4)); // 256
