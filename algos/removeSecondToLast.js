89cf970ba52659d39c043baf7c8c860cac814d2c
Dana Sevcik <danasevcik@hotmail.com>
2020-01-08 09:41:36 -0500

    commented solution for remove second to last

---
+++ algos/removeSecondToLast.js
@@ -0,0 +1,7 @@
+// given a linked list
+// return the second to last ele
+
+// create two variables
+// one for current and one for previous
+// iterate through linked list and keep track of the current/previous
+// when the current's next property is null, return the previous
