6b987d5671d118366487d8facf7cf7c497ebbde7
Dana Sevcik <danasevcik@hotmail.com>
2020-01-10 13:55:28 -0500

    brute force commented solution product arr

---
+++ algos/productArr.js
@@ -0,0 +1,8 @@
+// given an arr of integers
+// return an arr where each element is the product of all the other numbers aside from itself
+// input: [1,2,3,4]
+// output: [24,12,8,6]
+
+// map through arr
+// create product variable where you store the product
+// iterate again and multiply all where i does not equal j
