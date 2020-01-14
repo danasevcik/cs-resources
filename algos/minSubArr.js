4d0b9fadf6094aa050fa6fe5e8855e2958186d17
Dana Sevcik <danasevcik@hotmail.com>
2020-01-14 10:41:02 -0500

    commented solution for min sub arr

---
+++ algos/minSubArr.js
@@ -0,0 +1,9 @@
+// given an arr and target value
+// find the minimum length of contiguous subarr that sums to target
+// return that length
+
+// create sum var and length var
+// iterate over arr
+// add next values
+// if the sum is reached or there are no more, move onto next
+// otherwise if the sum is met, check if length is lower
