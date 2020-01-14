0734c830b8998225a3b522a4ab63ecf924979b34
Dana Sevcik <danasevcik@hotmail.com>
2020-01-14 10:57:53 -0500

    O(n^2) solution to min sub arr

--- algos/minSubArr.js
+++ algos/minSubArr.js
@@ -7,3 +7,36 @@
 // add next values
 // if the sum is reached or there are no more, move onto next
 // otherwise if the sum is met, check if length is lower
+
+function minSubArr(arr, target) {
+  let lowestLength = 0;
+  let currentLength = 0;
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let current = arr[i];
+    console.log(current);
+    sum += current;
+    currentLength++;
+
+
+    while (sum <= target && arr[i+1]) {
+      j = i + 1
+      sum += arr[j]
+      currentLength++;
+      console.log('sum', sum);
+      console.log('currentLength', currentLength);
+      if (sum >= target) {
+        if (!lowestLength || currentLength < lowestLength) {
+          lowestLength = currentLength;
+        }
+      }
+    }
+    currentLength = 0;
+    sum = 0;
+  }
+
+  return lowestLength;
+}
+
+console.log(minSubArr([2,3,1,2,4,3], 7)); // 2
