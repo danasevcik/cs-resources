3c49e9b17c228540bcf90209e4502b35cbc14e82
Dana Sevcik <danasevcik@hotmail.com>
2020-01-17 10:06:53 -0500

    commented solution for single num arr

---
+++ algos/singleNum.js
@@ -0,0 +1,9 @@
+// given arr of numbers
+// exactly 2 numbers appear only once in the arr
+// return an arr of the two unique numbers
+// [1,2,1,3,2,5] => [3,5]
+
+// iterate through the arr and create a count obj
+// add each number to the obj and keep track of the count
+// after the initial loop, look through the count obj
+// only add numbers to the output arr if the count is 1
