c2a8c6043e1bfdc06a0524e4ca643764d6eeefc5
Dana Sevcik <danasevcik@hotmail.com>
2020-01-27 12:51:22 -0500

    commented solution for bulb switch

---
+++ algos/bulbSwitch.js
@@ -0,0 +1,11 @@
+// n bulbs and all are initially off
+// on the first run, you turn on all bulbs
+// on the second run, you toggle every second bulb
+// for every ith round, you toggle every ith bulb
+// for the nth round, you toggle only the last bulb
+// how many bulbs are on after n rounds
+
+// create an arr of objects on the first pass
+// have a lit key - toggle this boolean
+// [{1: bulb, lit: false}, {2: bulb, lit: false}, {3: bulb, lit: false}]
+// continue iterating and toggle by x
